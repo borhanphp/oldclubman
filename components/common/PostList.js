f8fcd07f4839e2bff25d75af0bcbf82a6cf23fcf
Borhan Uddin <borhanidb@gmail.com>
2025-05-15 15:57:36 +0600

    updated

--- components/common/PostList.js
+++ components/common/PostList.js
@@ -681,7 +681,7 @@ const PostList = ({postsData}) => {
               <div className="flex items-center gap-2">
                 <div className="w-10 h-10 rounded-full overflow-hidden">
                   <img
-                    src={process.env.NEXT_PUBLIC_FILE_PATH + basicPostData?.image}
+                    src={process.env.NEXT_PUBLIC_CLIENT_FILE_PATH + basicPostData?.image}
                     className="w-full h-full object-cover"
                   />
                 </div>
