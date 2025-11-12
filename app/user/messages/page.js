d19fa589912587dfdde4ce2cfa7ae30f98269c58
borhanphp <borhanidb@gmail.com>
2025-11-12 17:41:45 +0600

    updated

--- app/user/messages/page.js
+++ app/user/messages/page.js
@@ -2,13 +2,15 @@
 
 import CommonLayout from '@/components/common/CommonLayout';
 import MessagingContent from '@/views/message/feed';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 export default function MessagingPage() {
   return (
    <CommonLayout>
      <div className="fixed top-[64px] left-0 right-0 bottom-0 w-screen h-[calc(100vh-64px)]  z-[5]">
-      <MessagingContent />
+      <Suspense fallback={<div className="flex items-center justify-center h-full">Loading messages...</div>}>
+        <MessagingContent />
+      </Suspense>
     </div>
    </CommonLayout>
   );
