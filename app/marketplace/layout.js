7778e325dc91dabab9f347fd45a234ad083fc2f4
Borhan Uddin <borhanidb@gmail.com>
2026-01-04 16:55:59 +0600

    udpated

--- app/marketplace/layout.js
+++ app/marketplace/layout.js
@@ -2,16 +2,19 @@
 
 import SocialNavbar from "@/components/common/SocialNavbar";
 import { ChatBoxProvider } from "@/contexts/ChatBoxContext";
+import { PostCommentsProvider } from "@/contexts/PostCommentsContext";
 import GlobalChatBox from "@/components/common/GlobalChatBox";
 
 export default function MarketplaceLayout({ children }) {
   return (
     <ChatBoxProvider>
-      <div className="min-h-screen bg-gray-50">
-        <SocialNavbar />
-        <div className="w-full">{children}</div>
-        <GlobalChatBox />
-      </div>
+      <PostCommentsProvider>
+        <div className="min-h-screen bg-gray-50">
+          <SocialNavbar />
+          <div className="w-full">{children}</div>
+          <GlobalChatBox />
+        </div>
+      </PostCommentsProvider>
     </ChatBoxProvider>
   );
 }
--- components/nfc/NFCLayout.js
+++ components/nfc/NFCLayout.js
@@ -3,18 +3,21 @@
 import React from 'react';
 import SocialNavbar from '@/components/common/SocialNavbar';
 import { ChatBoxProvider } from '@/contexts/ChatBoxContext';
+import { PostCommentsProvider } from '@/contexts/PostCommentsContext';
 import GlobalChatBox from '@/components/common/GlobalChatBox';
 
 const NFCLayout = ({ children }) => {
   return (
     <ChatBoxProvider>
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50">
-        <SocialNavbar />
-        <main className="w-full">
-          {children}
-        </main>
-        <GlobalChatBox />
-      </div>
+      <PostCommentsProvider>
+        <div className="min-h-screen bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50">
+          <SocialNavbar />
+          <main className="w-full">
+            {children}
+          </main>
+          <GlobalChatBox />
+        </div>
+      </PostCommentsProvider>
     </ChatBoxProvider>
   );
 };
